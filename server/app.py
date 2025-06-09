4436e3b01a5664682b4cf58a187e86ce6722e50f
Richard Wasonga <rowwasonga@gmail.com>
2025-06-09 20:40:12 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, current_app
+from flask import Flask, request, current_app, g, make_response
 
 app = Flask(__name__)
 
@@ -22,6 +22,6 @@ def index():
     headers = {}
 
     return make_response(response_body, status_code, headers)
-    
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
